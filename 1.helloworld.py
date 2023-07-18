9ba475ed73821c044fef677245ba69e6ea512194
mjavadpur <mjavadpur@gmail.com>
2023-07-18 18:05:50 +0330

    add images and 8, 9, 10

--- 1.helloworld.py
+++ 1.helloworld.py
@@ -3,6 +3,6 @@ import gradio as gr
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(fn=greet, inputs="text", outputs="text", css=".gradio-container {background-color: red}")
     
 demo.launch()
