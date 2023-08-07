af2464aaef70c800426f18bb95edb83c8b3b3117
mjavadpur <mjavadpur@gmail.com>
2023-08-07 20:17:23 +0330

    change discord token

--- GradioAPI/ChatBot.py
+++ GradioAPI/ChatBot.py
@@ -2,5 +2,5 @@
 from gradio_client import Client
 import gradio_client as grc
 grc.Client("gradio/calculator").deploy_discord(\
-    discord_bot_token='MTEzODAzMjgzNjEwMDc1NTUwNg.G2PWQ0.c8RExjGBnrjpSWUq5GxXEDyQQSQE7fK5OGcxRA', \
+    discord_bot_token='.0.MTEzODAzMjgzNjEwMDc1NTUwNg.G2PWQ0.0.c8RExjGBnrjpSWUq5GxXEDyQQSQE7fK5OGcxRA', \
         api_names='/chat')
