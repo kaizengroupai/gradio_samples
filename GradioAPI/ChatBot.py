4c4ab56cb66b7d66cb21ba5817327ae56f3ca137
mjavadpur <mjavadpur@gmail.com>
2023-08-08 09:57:06 +0330

    ppt file 002 version

--- GradioAPI/ChatBot.py
+++ GradioAPI/ChatBot.py
@@ -4,3 +4,8 @@ import gradio_client as grc
 grc.Client("gradio/calculator").deploy_discord(\
     discord_bot_token='.0.MTEzODAzMjgzNjEwMDc1NTUwNg.G2PWQ0.0.c8RExjGBnrjpSWUq5GxXEDyQQSQE7fK5OGcxRA', \
         api_names='/chat')
+
+
+<iframe
+	src="https://mjavadpur-basicdip.hf.space" frameborder="0" width="850" height="450"
+></iframe>
