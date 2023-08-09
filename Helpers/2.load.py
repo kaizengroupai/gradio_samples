456cdbaab85a3bc8259cd3d4986776ddd0ec5cc4
Mohammad Javadpur <mjavadpur@gmail.com>
2023-08-09 10:12:06 +0330

    ppt 003 version (joined mj and vahid slides)

--- Helpers/2.load.py
+++ Helpers/2.load.py
@@ -1,3 +1,16 @@
 import gradio as gr
 demo = gr.load("gradio/tax_calculator", src="spaces")
 demo.launch()
+
+
+
+#1-Create a new virtual environment (recommended):
+
+python -m venv gradio-env
+
+
+Activate the virtual environment:
+
+On Windows:
+
+gradio-env\Scripts\activate.bat
